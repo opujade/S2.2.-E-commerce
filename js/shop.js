c682a0db0f1c0c3ffe22f4f6bc4e520a6d24dbbd
opujade <uripujade@gmail.com>
2024-03-14 11:07:12 +0100

    Terminado ejercicio 5. Añadido actualizar icono cart y modal de forma dinámica.

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,5 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
+const products = [
   {
     id: 1,
     name: 'cooking oil',
@@ -70,9 +70,12 @@ var products = [
 // ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-var cart = [];
+const cart = [];
 
-var total = 0;
+let total = 0;
+
+// Elements
+const cartList = document.getElementById('cart_list');
 
 // Exercise 1
 function buy(id) {
@@ -88,10 +91,11 @@ function buy(id) {
   } else {
     cart.push(products[productIndex]);
     cart[cart.length - 1].quantity = 1;
-    cart[cart.length - 1].subtotal = cart[cart.length - 1].price;
+    cart[cart.length - 1].subtotal = cart[cart.length - 1].price; // Crear la propiedad subtotal para después poder operar con el precio total sin modificar el precio original.
   }
-//   applyPromotionsCart();
-//   calculateTotal();
+  applyPromotionsCart();
+  calculateTotal();
+  printCart();
 }
 
 // Exercise 2
@@ -100,6 +104,8 @@ function cleanCart() {
   for (let i = 0; i < cartLength; i++) {
     cart.pop();
   }
+  total = 0;
+  printCart();
 }
 
 // Exercise 3
@@ -107,13 +113,14 @@ function calculateTotal() {
   total = 0;
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].subtotalWithDiscount == undefined) {
+      // Si existe un subtotal con descuento tomará el descuento para calcular el precio total. Así también podemos saber el subtotal sin descuento por si lo necesitásemos en un futuro (poner un precio tachado, por ejemplo).
       total += cart[i].subtotal;
     } else {
       total += cart[i].subtotalWithDiscount;
     }
   }
-//   console.log(cart);
-//   console.log(total);
+  //   console.log(cart);
+  //   console.log(total);
 }
 
 // Exercise 4
@@ -121,16 +128,53 @@ function applyPromotionsCart() {
   // Apply promotions to each item in the array "cart"
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].quantity >= 10) {
-        cart[i].subtotalWithDiscount = cart[i].subtotal * .7;
+      cart[i].subtotalWithDiscount = cart[i].subtotal * 0.7;
     } else if (cart[i].quantity >= 3) {
-        cart[i].subtotalWithDiscount = cart[i].subtotal * .8;
+      cart[i].subtotalWithDiscount = cart[i].subtotal * 0.8;
     }
   }
 }
 
 // Exercise 5
 function printCart() {
-  // Fill the shopping cart modal manipulating the shopping cart dom
+  cartList.innerHTML = '';
+  for (let i = 0; i < cart.length; i++) {
+    /* Crear tabla:
+        <tr>
+            <th scope="row">Nombre producto</th>
+            <td>Precio</td>
+            <td>Cantidad</td>
+        </tr> 
+    */
+    let tableRow = document.createElement('tr');
+    let productName = document.createElement('th');
+    productName.scope = 'row';
+    productName.innerHTML = cart[i].name;
+    let price = document.createElement('td');
+    price.innerHTML = cart[i].price;
+    let quantity = document.createElement('td');
+    quantity.innerHTML = cart[i].quantity;
+    let totalProduct = document.createElement('td');
+    // Devuelve subtotal con descuento si existe. Sino devuelve el subtotal sin descuento.
+    totalProduct.innerHTML = cart[i].subtotalWithDiscount
+      ? cart[i].subtotalWithDiscount.toFixed(2)
+      : cart[i].subtotal.toFixed(2);
+
+    tableRow.appendChild(productName);
+    tableRow.appendChild(price);
+    tableRow.appendChild(quantity);
+    tableRow.appendChild(totalProduct);
+    cartList.appendChild(tableRow);
+  }
+
+  // Modificar el número de productos en el botón Cart de arriba a la derecha.
+  let totalQuantity = 0;
+  for (let i = 0; i < cart.length; i++) {
+    totalQuantity += cart[i].quantity;
+  }
+  document.getElementById('count_product').innerHTML = totalQuantity;
+  // Dibuja el precio total en el modal.
+  document.getElementById('total_price').innerHTML = total.toFixed(2);
 }
 
 // ** Nivell II **
